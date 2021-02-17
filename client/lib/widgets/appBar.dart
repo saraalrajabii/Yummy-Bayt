8a511c6a85ebbd3f8bb85e730802f7a3a4bb7795
khawla ilahi <khawla.ilahi1@gmail.com>
2021-02-17 15:14:22 +0400

    homePageBody+carousel

--- client/lib/widgets/appBar.dart
+++ client/lib/widgets/appBar.dart
@@ -16,13 +16,13 @@ class IAppBar extends PreferredSize {
     return AppBar(
       // Here we take the value from the MyHomePage object that was created by
       // the App.build method, and use it to set our appbar title.
-      title: Text("Yummy Bayt",
+      title: Text("YUMMY BAYT",
           style: TextStyle(
-              fontWeight: FontWeight.bold,
+              // fontWeight: FontWeight.bold,
               fontSize: 25,
-              letterSpacing: 2,
+              letterSpacing: 4,
               color: Colors.white,
-              fontFamily: 'BungeeInline')),
+              fontFamily: 'Lalezar')),
       centerTitle: true,
       backgroundColor: Colors.yellow[800],
     );
